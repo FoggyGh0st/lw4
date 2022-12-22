a984ca005eced97044338604bc14b2cc08c342d8
FoggyGh0st <113252795+FoggyGh0st@users.noreply.github.com>
2022-12-22 17:39:17 +0300

    first commit

---
+++ tests/DataTest.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'src/lw4.php';
+use PHPUnit\Framework\TestCase;
+
+class DateTest extends TestCase{
+    private $date;
+    protected function setUp(): void{
+        $this->date =new Date(1, 2, 2001);
+    }
+    public function testPow():void{
+        $date2 = new Date(1, 4, 2001);
+        $this->assertEquals($this->date->diffDay($date2), 62);
+    }
+    public function testMinus(): void{
+        $this->assertEquals($this->date->minusDay(4), '24.5.2003');
+    }
+    public function testDayOfWeek(): void{
+        $this->assertEquals($this->date->getDateOfWeek(), 'Tuesday');
+    }
+    public function testFormat(): void{
+        $this->assertEquals($this->date->format('ru'), '28.1.2001');
+    }
+}
